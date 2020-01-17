31da0b1ee137d97e4ba3e3eb614c444f1a112bb7
echozr <307057322@qq.com>
2020-01-17 17:48:47 +0800

    新增热门搜索

--- server/interface/search.js
+++ server/interface/search.js
@@ -47,5 +47,46 @@ router.get('/top', async (ctx) => {
         }
     }
 })
+// 热门搜索
+router.get('/hotPlace', async (ctx) => {
+    const city = ctx.store ? ctx.store.geo.position.city : ctx.query.city
+    // try {
+    //     const result = await POI.find({
+    //         city,
+    //         type: ctx.query.type || '景点'
+    //     }).limit(10)
+    //     ctx.body = {
+    //         code: 0,
+    //         result: result.map((item) => {
+    //             return {
+    //                 name: item.name,
+    //                 type: item.type
+    //             }
+    //         })
+    //     }
+    // } catch (e) {
+    //     ctx.body = {
+    //         code: -1,
+    //         result: []
+    //     }
+    // }
+    const { status, data } = await axios.get('http://cp-tools.cn/search/hotPlace', {
+        params: {
+            city,
+            sign
+        }
+    })
+    if (status === 200 && data) {
+        ctx.body = {
+            code: 0,
+            result: data.result
+        }
+    } else {
+        ctx.body = {
+            code: -1,
+            result: []
+        }
+    }
+})
 
 export default router
